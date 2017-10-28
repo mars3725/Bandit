3b653e6d075eb9dcea1d1a335cf8032d7e7e8963
mars3725 <matt1298>
2017-10-28 04:33:28 -0400

    fixed web auth

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import spotipy
 import spotipy.util as util
 
-scope = 'user-library-read'
-
 token = util.prompt_for_user_token(username='mars3725',
-            scope='user-library-read',
+            scope='user-top-read',
             client_id='1e86238a2330483fad99b6c952b9c874',
             client_secret='ecbe8234150b4c518940eaee14f8ff36',
             redirect_uri='http://localhost:8888/callback')
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print(track['name'] + ' - ' + track['artists'][0]['name'])
+    sp.trace = False
+    results = sp.current_user_top_artists(time_range='long_term', limit=50)
+    for i, item in enumerate(results['items']):
+        print(i, item['name'])
 else:
     print("Can't get token")
