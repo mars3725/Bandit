5e3d5381d3293870aea6a8c2cd0fb69605c802b2
mars3725 <matt1298>
2017-10-28 11:36:31 -0400

    more OAuth stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import oauth2
 from requests_oauthlib import OAuth2Session
 from requests_oauthlib.compliance_fixes import facebook_compliance_fix
-import facebook
+import facebook as fb
+import requests
+from oauthlib.oauth2 import BackendApplicationClient
 
 #Login Info
 spotifyClient = '1e86238a2330483fad99b6c952b9c874'
@@ -10,28 +11,14 @@ redirect_uri = 'https://localhost/'
 facebookClient = '294072021106314'
 facebookSecret = 'de82f75a2cd78c875479920b877'
 
-oauth = OAuth2Session(spotifyClient, redirect_uri=redirect_uri, scope='user-top-read')
-authorization_url, state = oauth.authorization_url('https://accounts.spotify.com/authorize')
-
+spotify = OAuth2Session(spotifyClient, redirect_uri=redirect_uri, scope=['user-top-read'])
+authorization_url, state = spotify.authorization_url('https://accounts.spotify.com/authorize')
 print('Authorize Spotify at ', authorization_url)
 authorization_response = input('Enter the full callback URL: ')
 
-# OAuth endpoints given in the Facebook API documentation
-authorization_base_url = 'https://www.facebook.com/dialog/oauth'
-token_url = 'https://graph.facebook.com/oauth/access_token'
-
-fb = facebook_compliance_fix(OAuth2Session(facebookClient, redirect_uri=redirect_uri))
-
-# Redirect user to Facebook for authorization
-authorization_url, state = fb.authorization_url(authorization_base_url)
-print('Authorize Facebook at ', authorization_url)
-
-# Get the authorization verifier code from the callback url
-redirect_response = input('Paste the full redirect URL here:')
-
-# Fetch the access token
-facebook.fetch_token(token_url, client_secret=facebookSecret, authorization_response=redirect_response)
-
-# Fetch a protected resource, i.e. user profile
-r = facebook.get('https://graph.facebook.com/me?')
-print(r.content)
+client = BackendApplicationClient(client_id=spotifyClient)
+spotify = OAuth2Session(client=client)
+token = spotify.fetch_token('https://accounts.spotify.com/api/token', client_id=spotifyClient, client_secret=spotifySecret)
+usersArtists = spotify.request('GET', url='https://api.spotify.com/v1/me/top/{type}',
+                               data={'type': 'artists', 'limit': 5, 'time_range': 'long_term'})
+print(usersArtists)
