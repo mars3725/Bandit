6fcb6f3659ab5c6f619e028739bd289023a79562
mars3725 <matt1298>
2017-10-28 00:09:58 -0400

    add auth

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import requests
 
-response = requests.get('', params=None, headers=None)
+params = {'client_id': '1e86238a2330483fad99b6c952b9c874', 'response_type': 'code', 'redirect_uri': 'http://localhost:8888/callback'}
+authResponse = requests.get('https://accounts.spotify.com/authorize', params=params, headers=None)
+
+print(authResponse)
+params = {'grant_type': 'authorization_code', 'code': ''}
+requests.post('https://accounts.spotify.com/api/token', params=params)
