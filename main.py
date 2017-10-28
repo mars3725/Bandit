69c0b5f29dfb2e541302cac540f5ed4dea1fdad5
mars3725 <matt1298>
2017-10-28 03:39:07 -0400

    fixed web auth

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import sys
 import spotipy
 import spotipy.util as util
 
 scope = 'user-library-read'
 
 token = util.prompt_for_user_token(username='mars3725',
-                scope='user-library-read',
-                client_id='1e86238a2330483fad99b6c952b9c874',
-                client_secret='ecbe8234150b4c518940eaee14f8ff36',
-                redirect_uri='http://localhost:8888/callback')
+            scope='user-library-read',
+            client_id='1e86238a2330483fad99b6c952b9c874',
+            client_secret='ecbe8234150b4c518940eaee14f8ff36',
+            redirect_uri='http://localhost:8888/callback')
 
 if token:
     sp = spotipy.Spotify(auth=token)
@@ -17,4 +16,4 @@ if token:
         track = item['track']
         print(track['name'] + ' - ' + track['artists'][0]['name'])
 else:
-    print("Can't get token for", username)
+    print("Can't get token")
