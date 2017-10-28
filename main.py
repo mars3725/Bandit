93a39867fb9f03e1f3a06204f4e6a404ca076484
mars3725 <matt1298>
2017-10-28 08:19:29 -0400

    working on facebook login

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ if token:
     print(relatedArtists)
 else:
     print("Can't get token")
+
+import facebook
+accessToken = 'EAAELdPpbPooBAB2Oea8GQFHautZCaUgknOVeNxo4b43xsA37nYaB5nazj1w8VsXbcYZBnjrZCm6o4ZCZAoV0x2YyXk9gE1yXH1kiFNtWvvqmyt7mQaZCZBwZBxDABHqh6lZCRwCDbh5ZAZAcd9Kf2NtoB8Dy3hotZA1I0VX9IyyQbRpKGAYYY2Msr12tCWKS2GvwtF8ZD'
+graph = facebook.GraphAPI(access_token=accessToken, version="2.10")
+
+app_id = 1231241241
+canvas_url = 'https://domain.com/that-handles-auth-response/'
+perms = ['manage_pages','publish_pages']
+fb_login_url = graph.auth_url(app_id, canvas_url, perms)
+print(fb_login_url)
